6a5b8b34a728b5b8d1757873761bf805f6a4155a
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-11-12 18:24:56 +0100

    remove the msgToPacket from process. for this impl there is no need for broker but interface is there for general use.

--- src/main/java/artronics/chaparMini/Chapar.java
+++ src/main/java/artronics/chaparMini/Chapar.java
@@ -48,11 +48,15 @@ public class Chapar implements Runnable
     @Subscribe
     public void recievePacketHandler(MessageReceivedEvent event)
     {
-        List<List<Integer>> messages = convertor.generatePackets(event.getPacket());
+        //there is no need for conversion with this implementation
+        //TODO create msgToPacket for general use
+//        List<List<Integer>> messages = convertor.generatePackets(event.getPacket());
+//
+//        for (List<Integer> msg : messages) {
+//            receivedBuffer.add(msg);
+//        }
 
-        for (List<Integer> msg : messages) {
-            receivedBuffer.add(msg);
-        }
+        receivedBuffer.add(event.getPacket());
     }
 
     @Override
