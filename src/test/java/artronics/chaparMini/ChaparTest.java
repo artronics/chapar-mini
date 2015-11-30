52a46b14f45de9f85f7404acdc3fb0eac665a6e9
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-11-30 16:55:10 +0100

    fix the problem with package not found in maven build

--- src/test/java/artronics/chaparMini/ChaparTest.java
+++ src/test/java/artronics/chaparMini/ChaparTest.java
@@ -4,7 +4,6 @@ import artronics.chaparMini.broker.MessageToPacketConvertor;
 import artronics.chaparMini.connection.Connection;
 import artronics.chaparMini.connection.ConnectionStatusType;
 import artronics.chaparMini.connection.serialPort.SerialPortConnection;
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -35,7 +34,7 @@ public class ChaparTest
 
     MockConnection mockConnection = new MockConnection();
 
-    InputStream input = new ByteInputStream();
+    InputStream input;
     ByteArrayOutputStream output = new ByteArrayOutputStream();
 
     @Spy
